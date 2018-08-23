ab2f222165d5086519222a584fdfeae10f6add94
shepherd_dc <shepherd_dc@aliyun.com>
2018-08-23 16:25:30 +0800

    'frontend-init'

---
+++ frontend/src/store/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import userInfo from './modules/userInfo'
+import user from './modules/user'
+import errorLog from './modules/errorLog'
+import getters from './getters'
+import createPersist from 'vuex-localstorage'
+import language from './modules/language'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  modules: {
+    errorLog,
+    user,
+    userInfo,
+    language
+  },
+  getters,
+  plugins: [createPersist({
+    namespace: 'namespace-for-state',
+    initialState: {},
+    expires: 7 * 24 * 60 * 60 * 1e3
+  })]
+})
+
+export default store
---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button class="btn btn-success">按钮</button>
+    <p v-for="(item, index) in tasks" :key="index">{{item.title}}</p>
+  </div>
+</template>
+<script>
+import {get} from '../utils/http'
+export default {
+  data () {
+    return {
+      tasks: []
+    }
+  },
+  created () {
+    get('http://172.16.5.54:5050/api/test').then(data => {
+      // console.log(data)
+      this.tasks = data.tasks
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+  mounted () {
+  }
+}
+</script>
