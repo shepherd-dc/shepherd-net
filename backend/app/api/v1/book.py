e97211bb7f5d31a4409c813dd3d45ec950c7862f
shepherd_dc <shepherd_dc@aliyun.com>
2018-08-24 10:05:06 +0800

    blueprint

---
+++ backend/app/api/v1/book.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+book = Blueprint('book', __name__)
+@book.route('/v1/book/get')
+def get_book():
+    return 'get book'
---
+++ backend/app/api/v1/user.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+user = Blueprint('user', __name__)
+@user.route('/v1/user/get')
+def get_user():
+    return 'get user'
