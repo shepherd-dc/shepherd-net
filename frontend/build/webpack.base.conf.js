bc99a621f3a928c059a425b66786e902caf2b0bc
shepherd_dc <shepherd_dc@aliyun.com>
2018-09-14 18:01:01 +0800

    ssr preparation

--- frontend/build/webpack.base.conf.js
+++ frontend/build/webpack.base.conf.js
@@ -4,6 +4,8 @@ const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 
+const VueClientPlugin = require('vue-server-renderer/client-plugin')
+
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
@@ -77,6 +79,9 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new VueClientPlugin()
+  ],
   node: {
     // prevent webpack from injecting useless setImmediate polyfill because Vue
     // source contains it (although only uses it if it's native).
