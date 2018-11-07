f5c97e3e4602a1cabdf8399f4c54f3f9e8b24d89
shepherd_dc <shepherd_dc@aliyun.com>
2018-11-07 09:55:20 +0800

    flask gunicorn api test

---
+++ backend/app/test/gunicorn_config.py
@@ -0,0 +1,7 @@
+
+glevel = 'debug'
+workers = 4
+bind = "0.0.0.0:5050"
+worker_class = 'sync'
+threads = 2
+
---
+++ backend/app/test/run.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, jsonify, make_response
+from functools import wraps
+ 
+app = Flask(__name__)
+
+def allow_cross_domain(fun):
+    @wraps(fun)
+    def wrapper_fun(*args, **kwargs):
+        rst = make_response(fun(*args, **kwargs))
+        rst.headers['Access-Control-Allow-Origin'] = '*'
+        rst.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+        allow_headers = "Referer,Accept,Origin,User-Agent"
+        rst.headers['Access-Control-Allow-Headers'] = allow_headers
+        return rst
+    return wrapper_fun
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+ 
+ 
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/api/test')
+@allow_cross_domain
+def api_test():
+    return jsonify({'tasks': tasks})
+
+@app.route('/api/test/<params>')
+@allow_cross_domain
+def params_test(params):
+    return jsonify({'params': params})
+ 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5050, threaded=True)
