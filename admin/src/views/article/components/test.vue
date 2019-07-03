fd3c9e329537c69a67d1018bf7f69783079220bb
shepherd_dc <shepherd_dc@aliyun.com>
2019-07-03 10:40:24 +0800

    use quilleditor in the same page, component editor usage to be settled

--- admin/src/views/article/components/test.vue
+++ admin/src/views/article/components/test.vue
@@ -71,15 +71,7 @@
         </el-form-item> -->
 
         <el-form-item class="quill-editor" prop="content">
-          <!-- <quill-editor :is-edit="isEdit" @deliverContent="handleContent"/> -->
-          <quill-editor
-            ref="myQuillEditor"
-            v-model="postForm.content"
-            :options="editorOption"
-            @ready="onEditorReady($event)"
-            @blur="onEditorBlur($event)"
-            @focus="onEditorFocus($event)"
-            @change="onEditorChange($event)"/>
+          <quill-editor :is-edit="isEdit" @deliverContent="handleContent"/>
         </el-form-item>
 
         <!-- <el-form-item prop="image_uri" style="margin-bottom: 30px;">
@@ -98,11 +90,7 @@
 </template>
 
 <script>
-// import QuillEditor from '@/components/Quilleditor/QuillEditor'
-import { quillEditor } from 'vue-quill-editor'
-import hljs from 'highlight.js' // 语法高亮
-// import 'highlight.js/styles/xcode.css'
-import 'highlight.js/styles/monokai-sublime.css'
+import QuillEditor from '@/components/Quilleditor/QuillEditor'
 import MDinput from '@/components/MDinput'
 import { fetchMenu } from '@/api/menu'
 import { fetchArticle, createArticle, editArticle } from '@/api/article'
@@ -122,7 +110,7 @@ const defaultForm = {
 
 export default {
   name: 'ArticleDetail',
-  components: { quillEditor, MDinput },
+  components: { QuillEditor, MDinput },
   props: {
     isEdit: {
       type: Boolean,
@@ -155,33 +143,8 @@ export default {
         title: [{ validator: validateRequire }],
         content: [{ validator: validateRequire }]
       },
-      menus: [],
-      editorOption: {
-        // some quill options
-        modules: {
-          toolbar: [
-            // [], // dropdown with defaults from theme
-            ['bold', 'italic', 'underline', 'strike', { 'color': [] }],
-            ['blockquote', 'code-block'],
-            [{ 'header': 1 }, { 'header': 2 }], // custom button values
-            [{ 'script': 'sub' }, { 'script': 'super' }], // superscript/subscript
-            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-            [{ 'indent': '-1' }, { 'indent': '+1' }], // outdent/indent
-            [{ 'direction': 'rtl' }] // text direction
-
-            // [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-            // [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-
-            // [{ 'font': [] }],
-            // [{ 'align': [] }],
-
-            // ['clean'] // remove formatting button
-          ],
-          syntax: {
-            highlight: text => hljs.highlightAuto(text).value // 语法高亮插件调用
-          }
-        }
-      }
+      tempRoute: {},
+      menus: []
     }
   },
   computed: {
@@ -228,7 +191,7 @@ export default {
     async fetchData(id) {
       const response = await fetchArticle(id)
       this.postForm = response.data
-      // this.$store.commit('FetchArticle', this.postForm)
+      this.$store.commit('FetchArticle', this.postForm)
 
       // Just for test
       // this.postForm.title += `   Article Id:${this.postForm.id}`
@@ -324,20 +287,6 @@ export default {
     handleContent(html) {
       // console.log(html)
       this.postForm.content = html
-    },
-    onEditorBlur(editor) {
-      // console.log('editor blur!', editor)
-    },
-    onEditorFocus(editor) {
-      // console.log('editor focus!', editor)
-    },
-    onEditorReady(editor) {
-      // console.log('editor ready!', editor)
-    },
-    onEditorChange({ quill, html, text }) {
-      // console.log('editor change!', quill, html, text)
-      // this.postForm.content = html
-      // this.$emit('deliverContent', html)
     }
   }
 }
@@ -371,23 +320,6 @@ export default {
     font-weight: 700;
     font-family: "Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, Arial, sans-serif";
   }
-
 }
 </style>
 
-<style lang="scss">
-  .ql-editor {
-    min-height: 500px;
-    overflow-y: auto;
-  }
-  .ql-toolbar.ql-snow {
-    padding: 0 0 0 8px!important;
-    border-radius: 4px 4px 0 0;
-  }
-  .ql-container.ql-snow {
-    border-radius: 0 0 4px 4px;
-  }
-  .ql-snow .ql-color-picker .ql-picker-label {
-    padding: 0 0 12px 4px;
-  }
-</style>
